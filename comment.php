afa001d02d2cafa7909922c8709ace344439ea2a
Kimjay123 <zhaynekhim@gmail.com>
2023-02-12 22:33:44 +0800

    Comment

---
+++ comment.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head></head>
+    <title>Hello Kim Jay!</title>
+    <body>
+    <?php
+        /*echo "Hi";
+        echo " Kim Jay";*/
+
+        //echo "Hi";
+        echo " Kim Jay";
+    ?>
+    </body>
+</html>
